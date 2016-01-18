ec390b785072c292ee9605f2fa6d0e910c254457
ruffle1986 <ftamas.mail@gmail.com>
2016-01-18 16:56:58 +0100

    refactor: readability, maintainability, testability.

--- bin/mk-release.js
+++ bin/mk-release.js
@@ -3,126 +3,75 @@
 'use strict';
 
 const workDir = process.cwd();
-
 const minimist = require('minimist');
 const pkg = require(workDir + '/package.json');
-const Log = require('log');
-const log = new Log();
-const _ = require('shelljs');
-const s3 = require('s3');
-
 const argv = minimist(process.argv.slice(2));
-
-function createRelease(owner, repo, version, options) {
-  options = options || {};
-  const token = options.token;
-  const url = `https://api.github.com/repos/${owner}/${repo}/releases`;
-  const payload = `"{\\"tag_name\\":\\"v${version}\\"}"`;
-  log.info(`post to ${url} with ${payload}...`);
-  let result = _.exec(
-    `curl \
-      -s \
-      -XPOST \
-      -d ${payload} \
-      -H 'Content-Type: application/json' \
-      -H "Authorization: token ${token}" \
-      ${url}`
-  ).output;
-  try {
-    result = JSON.parse(result);
-  } catch (error) {
-    result = {
-      message: error.message
-    };
-  }
-  if (!result.id) {
-    log.error(`failed to create release: ${result.message}`);
-    process.exit(1);
-  }
-  return result.id;
-}
-
-function uploadAsset(owner, repo, version, options) {
-    options = options || {};
-
-    if (options.aws) {
-
-      var client = s3.createClient({
-        s3Options: {
-          accessKeyId: options.aws.accessKey,
-          secretAccessKey: options.aws.secret,
-        },
-      });
-      var uploader = client.uploadFile({
-        localFile: `${workDir}/release/${version}.tgz`,
-        s3Params: {
-          Bucket: `${owner}-${repo}`,
-          Key: `${version}.tgz`,
-          ACL: 'authenticated-read',
-        },
-      });
-      uploader.on('error', function(err) {
-        log.error(`AWS S3: unable to upload: ${err.stack}`);
-      });
-      uploader.on('end', function() {
-        log.info('AWS S3: done uploading');
-      });
-
-    } else {
-      const token = options.github.token;
-      const url = `https://uploads.github.com/repos/${owner}/${repo}/releases/${release}/assets?name=${version}.tgz`;
-      log.info(`GITHUB: uploading to ${url}...`);
-      let result = _.exec(
-        `curl \
-          -s \
-          -XPOST \
-          -H 'Content-Type: application/gzip' \
-          -H "Authorization: token ${token}" \
-          --data-binary @${workDir}/release/${version}.tgz \
-          ${url}`
-      ).output;
-      try {
-        result = JSON.parse(result);
-      } catch (error) {
-        result = {
-          message: error.message
-        };
-      }
-      const asset = result.id;
-      if (!asset) {
-        log.info(`GITHUB: upload failed (${result.message}) let\`s try it again`);
-        log.info('GITHUB: ', result);
-        return uploadAsset(owner, repo, version, options);
-      }
-      return asset;
-    }
-}
 
 const owner = argv['gh-user'] || process.env.GITHUB_USER;
 const repo = argv['gh-repo'] || process.env.GITHUB_REPO;
 const githubToken = argv['gh-token'] || process.env.GITHUB_ACCESS_TOKEN;
-const awsAccessKey = argv['aws-access-key-id'] || process.env.AWS_ACCESS_KEY_ID;
-const awsSecret = argv['aws-secret-access-key'] || process.env.AWS_SECRET_ACCESS_KEY;
 const version = pkg.version;
 
-log.info(`create release v${version} on ${owner}/${repo}...`);
+if (githubToken) {
+  const createGithubRelease = require('../lib/create-github-release');
 
-const release = createRelease(owner, repo, version, {
-  token: githubToken
-});
+  createGithubRelease({
+    user: owner,
+    repo: repo,
+    accessToken: githubToken,
+    version: pkg.version
+  })
+  .then(function (data) {
 
-log.info(`release id: ${release}`);
+    if (!(data || {}).id) {
+      throw new Error('no release id');
+    }
 
-const uploadOptions = awsAccessKey && awsSecret ? {
-  aws: {
-    accessKey: awsAccessKey,
-    secret: awsSecret
+    console.log('uploading file to github');
+
+    const uploadToGithub = require('../lib/upload-to-github');
+
+    uploadToGithub({
+      user: owner,
+      repo: repo,
+      accessToken: githubToken,
+      version: pkg.version,
+      releaseId: data.id,
+      path: workDir
+    })
+      .then(function (response) {
+        if (response.id) {
+          console.log('asset uploaded: ', response.id);
+        }
+      })
+      .catch(function (err) {
+        throw new Error('upload failed: ', err.message);
+      });
+
+  })
+    .catch(function (err) {
+      console.error(err.message);
+    });
+} else {
+  const awsAccessKey = argv['aws-access-key-id'] || process.env.AWS_ACCESS_KEY_ID;
+  const awsSecret = argv['aws-secret-access-key'] || process.env.AWS_SECRET_ACCESS_KEY;
+  const bucket = argv['aws-bucket'] || process.env.AWS_BUCKET;
+
+  if (awsAccessKey && awsSecret) {
+    const uploadToAWS = require('../lib/upload-to-aws');
+
+    uploadToAWS({
+      accessKey: awsAccessKey,
+      secret: awsSecret,
+      version: version,
+      path: workDir,
+      bucket: bucket
+    })
+      .then(function () {
+        console.log('asset uploaded');
+      })
+      .catch(function (err) {
+        console.error(err.message);
+      });
   }
-} : {
-  github: {
-    token: githubToken
-  }
-};
-
-const asset = uploadAsset(owner, repo, version, uploadOptions);
-log.info(`asset uploaded: ${asset}`);
+}
