31199722394b5a4e97869e3fc4d5b21606aaa2c2
ruffle1986 <ftamas.mail@gmail.com>
2016-01-19 10:54:39 +0100

    feat: make it user-friendly

--- bin/mk-release.js
+++ bin/mk-release.js
@@ -6,11 +6,31 @@ const workDir = process.cwd();
 const minimist = require('minimist');
 const pkg = require(workDir + '/package.json');
 const argv = minimist(process.argv.slice(2));
+const chalk = require('chalk');
+const spinner = require('elegant-spinner');
+const logUpdate = require('log-update');
+const symbols = require('log-symbols');
 
 const owner = argv['gh-user'] || process.env.GITHUB_USER;
 const repo = argv['gh-repo'] || process.env.GITHUB_REPO;
 const githubToken = argv['gh-token'] || process.env.GITHUB_ACCESS_TOKEN;
 const version = pkg.version;
+
+const log = console.log.bind(console);
+const info = chalk.cyan;
+const error = msg => chalk.red(symbols.error, msg);
+const success = msg => chalk.green(symbols.success, msg);
+
+let interval;
+
+function startSpinner() {
+  const frame = spinner();
+  interval = setInterval(() => logUpdate(frame()), 50);
+}
+
+function stopSpinner() {
+  clearInterval(interval);
+}
 
 if (githubToken) {
   const createGithubRelease = require('../lib/create-github-release');
@@ -19,39 +39,59 @@ if (githubToken) {
     user: owner,
     repo: repo,
     accessToken: githubToken,
-    version: pkg.version
-  })
-  .then(function (data) {
+    version: pkg.version,
+    start: () => {
+      log(info('Creating github release...'));
+      startSpinner();
+    },
+    end: (err, data) => {
+      stopSpinner();
 
-    if (!(data || {}).id) {
-      throw new Error('no release id');
-    }
+      if (err) {
+        logUpdate(error(`Creating github release failed: ${err.message}`));
+        process.exit(1);
+      }
 
-    console.log('uploading file to github');
+      if (!(data || {}).id) {
+        logUpdate(error('Creating github release failed: No release id.'));
+        process.exit(1);
+      }
 
-    const uploadToGithub = require('../lib/upload-to-github');
+      logUpdate(success('Release has been created successfully.'));
 
-    uploadToGithub({
-      user: owner,
-      repo: repo,
-      accessToken: githubToken,
-      version: pkg.version,
-      releaseId: data.id,
-      path: workDir
-    })
-      .then(function (response) {
-        if (response.id) {
-          console.log('asset uploaded: ', response.id);
+      const uploadToGithub = require('../lib/upload-to-github');
+
+      uploadToGithub({
+        user: owner,
+        repo: repo,
+        accessToken: githubToken,
+        version: pkg.version,
+        releaseId: data.id,
+        path: workDir,
+
+        start: () => {
+          log(info('Uploading file to github...'));
+          log('next');
+          startSpinner();
+        },
+
+        end: (err, response) => {
+          stopSpinner();
+
+          if (err) {
+            logUpdate(error(`Upload failed: ${err.message}`));
+            process.exit(1);
+          }
+
+          if (response && response.id) {
+            logUpdate(success(`Asset has been uploaded: ${response.id}`));
+          }
         }
-      })
-      .catch(function (err) {
-        throw new Error('upload failed: ', err.message);
       });
-
+    }
   })
-    .catch(function (err) {
-      console.error(err.message);
-    });
+  .catch(err => logUpdate(error(err.message)));
+
 } else {
   const awsAccessKey = argv['aws-access-key-id'] || process.env.AWS_ACCESS_KEY_ID;
   const awsSecret = argv['aws-secret-access-key'] || process.env.AWS_SECRET_ACCESS_KEY;
@@ -65,13 +105,23 @@ if (githubToken) {
       secret: awsSecret,
       version: version,
       path: workDir,
-      bucket: bucket
-    })
-      .then(function () {
-        console.log('asset uploaded');
-      })
-      .catch(function (err) {
-        console.error(err.message);
-      });
+      bucket: bucket,
+
+      start: () => {
+        log(info('Uploading file to AWS S3...'));
+        startSpinner();
+      },
+
+      end: (err) => {
+        stopSpinner();
+
+        if (err) {
+          logUpdate(error(err.message));
+          process.exit(1);
+        } else {
+          logUpdate(success('Asset has been uploaded.'));
+        }
+      }
+    });
   }
 }
