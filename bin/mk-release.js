210ecd6019e0a486e87135df3b84fd3b3dbe9fe1
ruffle1986 <ftamas.mail@gmail.com>
2016-01-20 09:29:58 +0100

    fix: eslint fixes

--- bin/mk-release.js
+++ bin/mk-release.js
@@ -6,7 +6,9 @@ const chalk = require('chalk');
 const symbols = require('log-symbols');
 
 const fn = require('../lib');
+/*eslint-disable no-console*/
 const log = console.log.bind(console);
+/*eslint-enable no-console*/
 const info = chalk.cyan;
 const error = msg => chalk.red(symbols.error, msg);
 const success = msg => chalk.green(symbols.success, msg);
@@ -17,7 +19,7 @@ const argv = minimist(process.argv.slice(2));
 const githubToken = argv['gh-token'] || process.env.GITHUB_ACCESS_TOKEN;
 const githubUser = argv['gh-user'] || process.env.GITHUB_USER;
 const githubRepo = argv['gh-repo'] || process.env.GITHUB_REPO;
-const version = argv['version'] || process.env.RELEASE_VERSION;
+const version = argv.version || process.env.RELEASE_VERSION;
 const path = argv['asset-folder'] || process.env.RELEASE_ASSET_FOLDER;
 
 if (githubToken) {
@@ -36,8 +38,10 @@ if (githubToken) {
 
 } else {
 
-  const awsAccessKey = argv['aws-access-key-id'] || process.env.AWS_ACCESS_KEY_ID;
-  const awsSecret = argv['aws-secret-access-key'] || process.env.AWS_SECRET_ACCESS_KEY;
+  const awsAccessKey = argv['aws-access-key-id'] ||
+    process.env.AWS_ACCESS_KEY_ID;
+  const awsSecret = argv['aws-secret-access-key'] ||
+    process.env.AWS_SECRET_ACCESS_KEY;
 
   if (awsAccessKey && awsSecret) {
 
